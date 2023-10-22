f702cf78379cde32b8145f642a5cc4c3a85dfe64
Your Name <npwebapps@gmail.com>
2023-10-22 16:53:57 -0500

    logical

--- x.js
+++ x.js
@@ -2,18 +2,19 @@ const myLibrary = []
 
 const library = document.querySelector(".library")
 
-function Book(authName, bookName, genre, pages) {
+function Book(authName, bookName, genre, pages, readStatus) {
   this.authName = authName
   this.bookName = bookName
   this.genre = genre
   this.pages = pages
+  this.readStatus = readStatus
 }
 const authNameInput = document.querySelector(".authName")
 const submit = document.querySelector(".submit")
 const bookNameInput = document.querySelector(".bookName")
 const genreInput = document.querySelector(".genre")
 const pagesInput = document.querySelector(".pageNum")
-
+const readStatusInput = document.querySelector("#readStatus")
 function addBookToLibrary() {
   event.preventDefault()
 
@@ -21,8 +22,9 @@ function addBookToLibrary() {
   const bookName = bookNameInput.value
   const genre = genreInput.value
   const pages = pagesInput.value
+  const readStatus = readStatusInput.value
 
-  const newBook = new Book(authName, bookName, genre, pages)
+  const newBook = new Book(authName, bookName, genre, pages, readStatus)
 
   myLibrary.push(newBook)
 
@@ -44,13 +46,15 @@ function displayLibrary() {
     <p>Title: ${book.bookName}</p>
     <p>Genre: ${book.genre}</p>
     <p>Pages: ${book.pages}</p>
-    <p><input type="checkbox"> read? </p>
+    <p>Read Status: ${book.readStatus ? "Read" : "Unread"} </p>
     <button class ="remove-button" data-index="${index}">Remove</button>
+    <button class ="toggle-read-status-button" data-index="${index}" onclick="toggleReadStatus(${index})">Toggle Read Status</button>
   `
     library.appendChild(bookCard)
   })
 
   remove()
+  toggleReadStatus()
 }
 
 function remove() {
@@ -60,6 +64,11 @@ function remove() {
     const index = parseInt(button.getAttribute("data-index"))
     button.addEventListener("click", () => removeBook(index))
   })
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].readStatus = !myLibrary[index].readStatus
+  displayLibrary()
 }
 
 function removeBook(index) {
